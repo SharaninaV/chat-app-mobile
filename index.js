c18397bbe818a69d1379ac59e32daef211a30075
starlight3307 <viktoriya.sharanina@gmail.com>
2021-07-07 13:49:14 +0300

    Installed and set up sentry sdk. Doesn't work

--- index.js
+++ index.js
@@ -3,7 +3,13 @@
  */
 
 import {AppRegistry} from 'react-native';
+import * as Sentry from '@sentry/react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+Sentry.init({
+  dsn: 'https://df9e82dcdbd14fdcb36cbf5816359961@o913672.ingest.sentry.io/5851872',
+});
+Sentry.nativeCrash();
 
 AppRegistry.registerComponent(appName, () => App);
