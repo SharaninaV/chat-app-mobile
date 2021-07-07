a1595cd61bc013184953cd246dabf9a00ec29295
starlight3307 <viktoriya.sharanina@gmail.com>
2021-07-07 15:39:35 +0300

    Installed and set up react-native-router-flux

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import * as Sentry from '@sentry/react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 Sentry.init({
-  dsn: 'https://df9e82dcdbd14fdcb36cbf5816359961@o913672.ingest.sentry.io/5851872',
+  dsn: 'https://df9e82dcdbd14fdcb36cbf5816359961@o913672.ingest.sentry.io/5851872'
 });
 Sentry.nativeCrash();
 
